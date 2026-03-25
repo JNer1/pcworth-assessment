e31d5e4b5485ea38c0c5b492751b10c5144d564a
Javier Neri <javierneri81@gmail.com>
2026-03-26 00:41:16 +0800

    feat: seed with three products

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,24 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Product::insert([
+            ['name' => 'Wooting 60HE', 'price' => 11950, 'stock' => 10],
+            ['name' => 'Ninjutso Sora V2', 'price' => 5750, 'stock' => 25],
+            ['name' => 'RX 9060 XT', 'price' => 24750, 'stock' => 5],
         ]);
     }
 }
